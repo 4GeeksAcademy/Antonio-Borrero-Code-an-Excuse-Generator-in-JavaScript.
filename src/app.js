ba37177b863dca5eaf893e5502b2c3084ef5a504
Antonio Borrero <antojoseabad@gmail.com>
2025-05-15 12:03:54 +0000

    first commit excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["my dog ", "my grandad ", "the neighbour ", "superman "];
+  let action = ["ate ", "peed ", "burned ", "broke "];
+  let what = ["my homework ", "my car ", "the street "];
+  let when = ["this morning ", "yesterday ", "during lunch ", "the weekend "]
+
+  let whorandom = Math.floor(Math.random()*who.length);
+  let actionrandom = Math.floor(Math.random()*action.length);
+  let whatrandom = Math.floor(Math.random()*what.length);
+  let whenrandom = Math.floor(Math.random()*when.length);
+
+  document.querySelector("#excuse").innerHTML = 
+  (who[whorandom] + action[actionrandom] + what[whatrandom] + when[whenrandom]);
 };
